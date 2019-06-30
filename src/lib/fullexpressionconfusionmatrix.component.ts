ea4ed46731c3bb23748a8b5943f7c925425ba485
ricardo_grocha <1100662@isep.ipp.pt>
2019-06-30 18:11:47 +0100

    allow to install libraries

--- src/lib/fullexpressionconfusionmatrix.component.ts
+++ src/lib/fullexpressionconfusionmatrix.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-FullExpressionConfusionMatrix',
-  template: `
+    selector: 'lib-FullExpressionConfusionMatrix',
+    template: `
     <p>
-      fullexpressionconfusionmatrix works!
+      fullexpressioncon
     </p>
   `,
-  styles: []
+    styles: []
 })
 export class FullExpressionConfusionMatrixComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
