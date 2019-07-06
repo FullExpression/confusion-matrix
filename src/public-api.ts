d2968d42a2afab436bc78298fa3f51613c088d82
ricardo_grocha <1100662@isep.ipp.pt>
2019-07-06 07:09:37 +0100

    module creation

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of fullexpressionconfusionmatrix
  */
 
-export * from './lib/services/confusion.matrix.service';
-export * from './lib/modules/public.modules';
-export * from './lib/interface/confusion-matrix.interface';
+export * from './lib/services/public.services';
+export * from './lib/modules/public.modules'
+export * from './lib/interface/public.interfaces';
+export * from './lib/components/public.component';
