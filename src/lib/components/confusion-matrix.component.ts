e1aa3e83a2ddf5c226d5631101c05603eb700295
ricardo_grocha <1100662@isep.ipp.pt>
2019-07-06 20:21:48 +0100

    seperate content for each module created

--- src/lib/components/confusion-matrix.component.ts
+++ src/lib/components/confusion-matrix.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ConfusionMatrix } from '../interface/confusion-matrix.interface';
 
 @Component({
