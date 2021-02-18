e30d850a9a2c0c9828a09a6d6b584c7cebed9681
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-02-18 11:50:31 -0500

    Update documentation

--- projects/confusion-matrix/src/lib/components/confusion-matrix.models.ts
+++ projects/confusion-matrix/src/lib/components/confusion-matrix.models.ts
@@ -1,3 +1,5 @@
+import { NgIf } from "@angular/common";
+
 /**
  * Confusion matrix model.
  */
@@ -6,6 +8,20 @@ export class ConfusionMatrix {
     matrix = new Array<Array<number>>();
     normalizations = new Array<ConfusionMatrix>();
 
+    /**
+     * Creates new instance of confusion matrix.
+     * 
+     * @example
+     * <pre><code>
+     * const confusionMatrix = new ConfusionMatrix({
+     *   labels: ["Happiness", "Sadness"],
+     *   matrix:
+     *       [[1,2],
+     *       [3,4]]
+     *   });
+     * </code><pre> 
+     * @param confusionMatrix 
+     */
     constructor(confusionMatrix?: { labels: Array<string>, matrix: Array<Array<number>> }) {
         if (confusionMatrix) {
             this.labels = this.deepCopy(confusionMatrix.labels);
@@ -13,11 +29,25 @@ export class ConfusionMatrix {
         }
     }
 
+    /**
+     * Sets the confusion matrix value based on another confusion matrix.
+     * @param confusionMatrix The confusion matrix.
+     */
     setConfusionMatrix(confusionMatrix: ConfusionMatrix) {
         this.labels = this.deepCopy(confusionMatrix.labels);
         this.matrix = this.deepCopy(confusionMatrix.matrix);
     }
 
+    /**
+     * Normalizes all values of the matrix between two given values.
+     * 
+     * All normalizations will be saved in history and it is possible to revert last normalizations 
+     * by calling the function @see {@link ConfusionMatrix.revertNormalization}.
+     * 
+     * Can be util if you want to convert the values to percentage or between [0, 1].
+     * @param min Minimum value of the normalized range values [min, max].
+     * @param max Maximum value of the normalized range values [min, max]. 
+     */
     normalize(min: number = 0, max: number = 1) {
         const matrixMinMax = this.getMinAndMax();
         if (matrixMinMax) {
@@ -29,17 +59,28 @@ export class ConfusionMatrix {
                 }
             }
         }
+        this.normalize()
     }
 
-    revertNormalization() {
+    /**
+     * Reverts the last normalization occur getting the current confusion matrix.
+     * @return The confusion matrix object before the normalization. 
+     * If there is not any entry on the history, null will be returned.
+     */
+    revertNormalization(): ConfusionMatrix | null {
         if (this.normalizations.length > 0) {
             const cm = this.normalizations.pop();
             if (cm) {
                 this.setConfusionMatrix(cm);
             }
         }
+        return null;
     }
 
+    /** 
+     * Deep clones and return the current confusion matrix.
+     * @return The deep cloned confusion matrix object. 
+     * */
     clone(): ConfusionMatrix {
         const cloneObj = new ConfusionMatrix({
             labels: this.labels,
@@ -49,12 +90,16 @@ export class ConfusionMatrix {
         return cloneObj;
     }
 
-    private getMinAndMax(): { min: number, max: number } | undefined {
+    /**
+     * Gets the confusion matrix min and max value.
+     * @return Min and max confusion matrix value or null if not exists.
+     */
+    getMinAndMax(): { min: number, max: number } | null {
         let min = this.matrix[0][0];
         let max = this.matrix[0][0];
 
         if (!min || !max) {
-            return undefined;
+            return null;
         }
         for (let line of this.matrix) {
             for (let val of line) {
@@ -66,11 +111,19 @@ export class ConfusionMatrix {
         return { min, max };
     }
 
+    /**
+     * Deep copies a given object.
+     * @param object The object to be deep cloned.
+     * @return The deep cloned object.
+     */
     private deepCopy(object: any): any {
         return JSON.parse(JSON.stringify(object));
     }
 }
 
+/**
+ * Different sizes available for the confusion matrix visual component.
+ */
 export enum ConfusionMatrixSizes {
     Small = 'small',
     Medium = 'medium',
