ab9d4187335716f7430f5a9b09faad3ca0a9588d
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-02-23 17:54:27 -0500

    Solve unsuccessfully test coverage problems

--- projects/web-app/src/app/app.component.spec.ts
+++ projects/web-app/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('web-app app is running!');
-    });
 });
