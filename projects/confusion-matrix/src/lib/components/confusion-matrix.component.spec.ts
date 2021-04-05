94e1f79970eafcdd24891544076154110bc96155
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-05 17:09:24 -0400

    #27: Solve runnin tests problems

--- projects/confusion-matrix/src/lib/components/confusion-matrix.component.spec.ts
+++ projects/confusion-matrix/src/lib/components/confusion-matrix.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { DownloadService } from "../services/download.service";
 import { ImportService } from "../services/import.service";
 import { ConfusionMatrixComponent } from "./confusion-matrix.component";
+import { IntensityBarService } from "./intensity-bar/intensity-bar.service";
 
 describe("Statistic models test suite", () => {
 
@@ -12,7 +13,7 @@ describe("Statistic models test suite", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [ConfusionMatrixComponent],
-            providers: [DecimalPipe, DownloadService, ImportService]
+            providers: [DecimalPipe, DownloadService, ImportService, IntensityBarService]
         });
         fixture = TestBed.createComponent(ConfusionMatrixComponent);
         component = fixture.componentInstance;
