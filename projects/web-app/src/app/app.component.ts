bac14a6b8962a800657e151497744535cfb8a7f2
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-04-12 14:04:10 -0400

    Update website

--- projects/web-app/src/app/app.component.ts
+++ projects/web-app/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { version } from '../../../confusion-matrix/package.json';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    appVersion = version;
-}
+export class AppComponent { }
--- projects/web-app/src/app/components/home-page/home-page.component.ts
+++ projects/web-app/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
-
+import { version } from '../../../../../confusion-matrix/package.json';
 @Component({
     selector: 'home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+    appVersion = version;
 }
